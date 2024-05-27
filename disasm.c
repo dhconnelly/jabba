1acbe455f4285e6d2522b6447c943f05d4703437
Daniel Connelly <dhconnelly@gmail.com>
2024-05-27 22:14:34 +0200

    open input file, usage, etc

--- disasm.c
+++ disasm.c
@@ -1,6 +1,27 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void trace(char *msg) {
+    printf("LOG: %s\n", msg);
+}
+
+static void disasm(FILE *f) {
+    trace("disassembling...");
+}
 
 int main(int argc, char *argv[]) {
-    printf("hello, world!\n");
+    if (argc != 2) {
+        fprintf(stderr, "usage: disasm <class-file>\n");
+        exit(1);
+    }
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        fprintf(stderr, "disasm: failed to open class file %s: %s\n",
+                argv[1], strerror(errno));
+        exit(1);
+    }
+    disasm(f);
     return 0;
 }
