c7cf5856ef18c7051e024efd2bedce4b54afdb58
Daniel Connelly <dhconnelly@gmail.com>
2024-05-27 22:43:08 +0200

    read magic field

--- disasm.c
+++ disasm.c
@@ -1,15 +1,86 @@
+#include <assert.h>
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static void trace(char *msg) {
+/* logging */
+
+static void trace(const char *msg) {
     printf("LOG: %s\n", msg);
 }
 
+static void die(const char *msg) {
+    fprintf(stderr, "disasm: fatal error: %s\n", msg);
+    exit(1);
+}
+
+/*
+ * The Java class file format. For more details see:
+ * https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html
+ */
+
+typedef struct ClassFile {
+    uint32_t magic;
+} ClassFile;
+
+/* Utilities for reading binary data */
+
+typedef enum result {
+    RESULT_OK,
+    RESULT_EOF,
+    RESULT_ERR,
+} result;
+
+static const char *result_str(result r) {
+    switch (r) {
+        case RESULT_OK: return "ok";
+        case RESULT_EOF: return "eof";
+        case RESULT_ERR: return strerror(errno);
+    }
+    assert(0);
+}
+
+typedef struct buffer {
+    FILE *f;
+    int offset;
+} buffer;
+
+static void buffer_init(buffer *buf, FILE *f) {
+    buf->f = f;
+    buf->offset = 0;
+}
+
+static result buffer_uint32(buffer *buf, uint32_t *x) {
+    int c, i;
+    *x = 0;
+    for (i = 0; i < 4; i++) {
+        if ((c = fgetc(buf->f)) == EOF && ferror(buf->f)) {
+            return RESULT_ERR;
+        } else if (c == EOF) {
+            return RESULT_EOF;
+        } else {
+            buf->offset++;
+            *x = (*x << 8) | c;
+        }
+    }
+    return RESULT_OK;
+}
+
+/* Disassembling */
+
 static void disasm(FILE *f) {
     trace("disassembling...");
+    buffer buf;
+    buffer_init(&buf, f);
+    uint32_t magic;
+    result ok = buffer_uint32(&buf, &magic);
+    if (ok != RESULT_OK) die(result_str(ok));
+    printf("0x%X\n", magic);
 }
+
+/* Driver */
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
